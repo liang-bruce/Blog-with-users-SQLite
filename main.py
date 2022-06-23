9fb0e2e6b5662af31618d95dd2001939b185ac42
Bruce-0602 <liangjitian777@gmail.com>
2022-06-23 22:20:41 +1000

    update main.py to initialise DB

--- main.py
+++ main.py
@@ -114,7 +114,8 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
 
-# db.create_all()
+
+db.create_all()
 
 
 @app.route('/')
